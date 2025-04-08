4f1c0a7d7e72fb6f8ee6d353d31b27d79682cb88
vinkarn <96374145+vinkarn@users.noreply.github.com>
2025-04-09 01:27:04 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,6 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmR4aX8e6E9QLUUnXveze_sLjqAEeiWmI",
 
-  authDomain: "algorithmvisualiser-9fdcf.firebaseapp.com",
-
-  projectId: "algorithmvisualiser-9fdcf",
-
-  storageBucket: "algorithmvisualiser-9fdcf.firebasestorage.app",
-
-  messagingSenderId: "801095753020",
-
-  appId: "1:801095753020:web:238cf4753f7f7f33059239",
-};
 
 const app = initializeApp(firebaseConfig);
